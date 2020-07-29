3b29893b84a60f5775f6c57723f10d8bc7d8c159
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-29 17:22:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,8 @@ class Song
   def self.create_from_filename(file_name)
     song = self.new 
     song.name = file_name.split(" - ")[1].chomp(".mp3")
-    
+    song.artist_name = file_name.split(" - ")[0]
+    @@all << song
   end
   
 
