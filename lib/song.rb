0b28fb38452f3fc402a41dc22132bf28d9a3c448
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-29 16:53:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,49 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def self.create
+    song = Song.new 
+    song.save 
+    song 
+  end
+  
+  def self.new_by_name(song_name)
+    song = Song.new
+    song.name = song_name 
+    song
+  end
+  
+  def self.create_by_name(song_name)
+    song = Song.new 
+    song.name = song_name 
+    song.save
+    song
+  end
+  
+  def self.find_by_name(song_name)
+    @@all.find {|song| song.name == song_name}
+  end
+  
+  def self.find_or_create_by_name(song_name)
+      self.find_by_name(song_name) || self.create_by_name(song_name)
+  end  
+  
+  def  self.alphabetical 
+    @@all.sort_by do |song|
+      song.name 
+    end
+  end
+  
+  def self.new_from_file_name(file_name)
+    
+  end   
+  
+  
+  
+  
 
   def self.all
     @@all
@@ -9,5 +52,5 @@ class Song
   def save
     self.class.all << self
   end
-
+  
 end
